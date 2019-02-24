ff86c8ec043540561f12ca33d9a7c9cac402a651
kdsouza <keegan0415dsouza@gmail.com>
2019-02-23 19:48:57 -0800

    Cleaned up the code creating a button

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -10,56 +10,40 @@ function insertGameButton () {
   if (ytclass === null) return
   let topLevelButtons = ytclass.childNodes[1]
   let nodes = topLevelButtons.childNodes
-  if (nodes.length >= 3 && nodes[2].id !== 'gameToggleButton') {
+  if (nodes.length <= 4) {
     addGameButton(topLevelButtons)
   }
 }
 
+// Creates a deep copy of an html element, cloneNode causing errors with elements
+function elementDeepCopy (element) {
+  if (element === null) return null
+  let copyElement = element.cloneNode(false)
+  let copyChildList = []
+  for (let node of element.childNodes) {
+    let child = elementDeepCopy(node)
+    copyChildList.push(child)
+  }
+  copyElement.innerHTML = ''
+  for (let element of copyChildList) {
+    copyElement.appendChild(element)
+  }
+  return copyElement
+}
+
 function addGameButton (topLevelButtons) {
-  console.log('add button revamp pushing from vs code')
-  let youtubeToggleButtonRenderer = topLevelButtons.firstChild
-  // Youtube toogle button renderer
-  let youtubeToggleButtonRendererClone = youtubeToggleButtonRenderer.cloneNode(false)
-  youtubeToggleButtonRendererClone.id = 'gameToggleButton'
-  youtubeToggleButtonRendererClone.onclick = gameButtonClick
-  topLevelButtons.insertBefore(youtubeToggleButtonRendererClone, topLevelButtons.childNodes[2])
-  // a class = 'yt-simple-endpoint'
-  let aYtSimpleEndpoint = youtubeToggleButtonRenderer.firstChild
-  let aYtSimpleEndpointClone = aYtSimpleEndpoint.cloneNode(false)
-  youtubeToggleButtonRendererClone.append(aYtSimpleEndpointClone)
-  // Youtube icon button
-  let ytIconButton = aYtSimpleEndpoint.firstChild
-  let ytIconButtonClone = ytIconButton.cloneNode(false)
-  ytIconButtonClone.innerHTML = ''
-  aYtSimpleEndpointClone.append(ytIconButtonClone)
-  // Button
-  let button = document.createElement('button')
-  button.setAttribute('class', 'style-scope yt-icon-button')
-  button.setAttribute('id', 'button')
-  button.setAttribute('aria-label', 'Play game')
-  ytIconButtonClone.append(button)
-  // YT-icon
-  let youtubeIcon = document.createElement('yt-icon')
-  youtubeIcon.setAttribute('class', 'style-scope ytd-toggle-button-renderer')
-  button.append(youtubeIcon)
-  // SVG
-  let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  var gameButton = elementDeepCopy(topLevelButtons.firstChild)
+  // Modifies the copied icon element, to include an id and correct element
+  var alink = gameButton.firstChild
+  alink.removeChild(alink.childNodes[1])
+  gameButton.setAttribute('id', 'gameToggleButton')
+  gameButton.onclick = gameButtonClick
+  let svg = alink.firstChild.firstChild.firstChild.firstChild
   svg.setAttribute('id', 'game-icon-svg')
-  svg.setAttribute('viewBox', '0 0 24 24')
-  svg.setAttribute('preserveAspectRatio', 'xMidYMid meet')
-  svg.setAttribute('focusable', false)
-  svg.setAttribute('class', 'style-scope yt-icon')
-  svg.setAttribute('style', 'pointer-events: none; display: block; width: 100%; height: 100%;')
-  youtubeIcon.append(svg)
-  // G
-  let g = document.createElementNS('http://www.w3.org/2000/svg', 'g')
-  g.setAttribute('class', 'style-scope yt-icon')
-  svg.append(g)
-  // Path
-  let path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  let path = svg.firstChild.childNodes[1]
   path.setAttribute('d', 'M23 6H1v12h22V6zm-12 7H8v3H6v-3H3v-2h3V8h2v3h3v2zm4.5 2c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4-3c-.83 0-1.5-.67-1.5-1.5S18.67 9 19.5 9s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z')
-  path.setAttribute('class', 'style-scope yt-icon')
-  g.append(path)
+  // Insert before share button
+  topLevelButtons.insertBefore(gameButton, topLevelButtons.childNodes[2])
 }
 function gameButtonClick () {
   playing = !playing;
